15e1d469b4d73df3857050f3f8e86788708e8a42
Rotver <sanchizalejandro@gmail.com>
2026-05-25 10:51:23 +0200

    feature
    feature: cambios básicos para la funcionalidad de clientes

--- backend/src/appointments/appointments.module.ts
+++ backend/src/appointments/appointments.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Appointment } from './appointment.entity';
+import { Payment } from '../payments/payment.entity';
 import { AppointmentsController } from './appointments.controller';
 import { AppointmentsService } from './appointments.service';
 import { BusinessesModule } from '../businesses/businesses.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Appointment]), BusinessesModule],
+  imports: [TypeOrmModule.forFeature([Appointment, Payment]), BusinessesModule],
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
 })
